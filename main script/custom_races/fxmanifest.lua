b535d3ec0b0f43f8fa9ed082fdf6df64e4e0c910
taoletsgo <56316172+taoletsgo@users.noreply.github.com>
2025-09-05 00:41:37 +0800

    tweak(races): clean code

--- main script/custom_races/fxmanifest.lua
+++ main script/custom_races/fxmanifest.lua
@@ -1,38 +1,38 @@
-fx_version 'cerulean'
-game 'gta5'
+fx_version "cerulean"
+game "gta5"
 
-author 'Rockstar Games'
-description 'Races system'
-version '3.5.0'
+author "Rockstar Games"
+description "Races system"
+version "3.5.1"
 
 client_scripts {
-	'config/config_client.lua',
-	'client/xbox_controller.lua',
-	'client/translate_client.lua',
-	'client/function_client.lua',
-	'client/callback_client.lua',
-	'client/main_client.lua',
-	'client/races_data.lua',
-	'client/races_room.lua'
+	"config/config_client.lua",
+	"client/xbox_controller.lua",
+	"client/translate_client.lua",
+	"client/function_client.lua",
+	"client/callback_client.lua",
+	"client/main_client.lua",
+	"client/races_data.lua",
+	"client/races_room.lua"
 }
 
 server_scripts {
-	'config/config_server.lua',
-	'@oxmysql/lib/MySQL.lua',
-	'server/sql_server.lua',
-	'server/callback_server.lua',
-	'server/main_server.lua',
-	'server/races_data.lua',
-	'server/races_room.lua'
+	"config/config_server.lua",
+	"@oxmysql/lib/MySQL.lua",
+	"server/sql_server.lua",
+	"server/callback_server.lua",
+	"server/main_server.lua",
+	"server/races_data.lua",
+	"server/races_room.lua"
 }
 
-ui_page 'html/index.html'
+ui_page "html/index.html"
 
 files {
-	'html/index.html',
-	'html/css/*.css',
-	'html/js/*.js',
-	'html/fonts/*.*',
-	'html/img/*.*',
-	'html/sounds/*.*'
+	"html/index.html",
+	"html/css/*.css",
+	"html/js/*.js",
+	"html/fonts/*.*",
+	"html/img/*.*",
+	"html/sounds/*.*"
 }
